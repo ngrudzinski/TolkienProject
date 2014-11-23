880ac381570b650b9a0021dfb43e6cce163c419b
Nathaniel Grudzinski <n.grudzinski@siu.edu>
2014-11-22 20:57:34 -0600

    First commit

---
+++ mainScript.js
@@ -0,0 +1,17 @@
+var frodo=0;
+var gimli=0;
+var gandalf=0;
+var legolas=0;
+var sam=0;
+var gollum=0;
+var orc=0;
+var aragorn=0;
+var galadrial=0;
+
+function submit(){
+	if(document.getElementById("beer").checked){
+		sam+=1;
+		frodo+=1;
+		gimli+=1.5;
+	}
+}
